ab4d0d486817d6a0a99401d7b8e56f7e95e05b54
TranTanDat1162 <dattt11062002@gmail.com>
2024-07-09 14:17:20 +0700

    update suspense from react

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 
 // contact.page.tsx
 const ContactPage = () => {
@@ -11,23 +11,25 @@ const ContactPage = () => {
   const message = searchParam.get("message");
 
   return (
-    <div className="p-8 m-8 md:p-16 md:m-16">
-      <h1 className="text-3xl font-bold mb-4">Contact Submit</h1>
-      <div className="p-4 border-2">
-        <p className="text-lg mb-4">Name: {name}</p>
-        <p className="text-lg mb-4">Email: {email}</p>
-        <p className="text-lg">Message: {message}</p>
-      </div>
+    <Suspense>
+      <div className="p-8 m-8 md:p-16 md:m-16">
+        <h1 className="text-3xl font-bold mb-4">Contact Submit</h1>
+        <div className="p-4 border-2">
+          <p className="text-lg mb-4">Name: {name}</p>
+          <p className="text-lg mb-4">Email: {email}</p>
+          <p className="text-lg">Message: {message}</p>
+        </div>
 
-      <div className="flex justify-center float-right">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8"
-          onClick={() => window.history.back()}
-        >
-          Back
-        </button>
+        <div className="flex justify-center float-right">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8"
+            onClick={() => window.history.back()}
+          >
+            Back
+          </button>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
